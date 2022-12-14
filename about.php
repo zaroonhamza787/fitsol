5063caa4d8e43b21c6e2e44a0282f6dd02d3bdb6
zaroonhamza787 <120562650+zaroonhamza787@users.noreply.github.com>
2022-12-14 15:27:55 +0500

    Add files via upload

---
+++ about.php
@@ -0,0 +1,155 @@
+<!DOCtype html>
+<html>
+ <head>
+                <title>About</title>
+                <link rel="icon" type="image/x-icon" href="./img/favicon.ico">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <!-- CSS only -->
+                <link rel="stylesheet" href=".css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">    
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+                <link rel="stylesheet" href="./assets/style2.css">
+    <style>
+    a:link{
+      color:white;
+    
+    }
+    a:visited{
+      color:white;
+    }
+      </style>
+ </head>
+ <body>
+            <nav class="navbar fixed-top navbar-expand-lg p-md-3  ">
+        <a class="navbar-brand" href="index.php">FitSol</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+          <ul class="navbar-nav me-auto">
+            <li><a  class="nav-link" href="index.php">Home</a></li>
+                <li class="nav-item"><a  class="nav-link" href="about.php">About</a></li>
+                <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Services
+                </a>
+                <ul class="dropdown-menu   topnavdrop" aria-labelledby="navbarDarkDropdownMenuLink">
+                  <li><a class="dropdown-item" href="#">Managed IT</a></li>
+                  <li><a class="dropdown-item" href="#">Expertise IT</a></li>
+                  <li><a class="dropdown-item" href="#">CyberSecurity</a></li>
+                </ul>
+              </li>
+              <li class="nav-item"><a  class="nav-link" href="contact.php">Contact us</a></li>
+            </ul>
+          </div>
+        </div>
+        </nav>
+            <header>
+             <img class="d-block w-100" src="./img/why1.jpg  " class="d-block w-100"alt="First slide">
+          </header>
+        
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <h1 class="h1-text pt-5 text-center">Our Values</h1>
+                        <p class="pt-5   text-justify">These seven principles highlight our fundamental beliefs as 
+                            a team and as a business. Our core values attract and unite 
+                            individuals focused on building a stable, desirable work 
+                            environment and providing exceptional solutions we are proud of.</p>
+                    </div>
+                    <div class="col-sm-6 d-table pt-5 pb-5 bg">
+                      <a href="#"><img src="./img/s1.png"  alt=""></a>
+                      <a href="#"><img src="./img/s1.png"  alt=""></a>
+                      <a href="#"><img src="./img/s1.png"  alt=""></a>
+                      <a href="#"><img src="./img/s1.png"  alt=""></a>
+                      <a href="#"><img src="./img/s1.png"  alt=""></a>  
+                      <a href="#"><img src="./img/s1.png"  alt=""></a>  
+                      <a href="#"><img src="./img/s1.png"  alt=""></a>
+                      <a href="#"><img src="./img/s1.png"  alt=""></a>
+                      <a href="#"><img src="./img/s1.png"  alt=""></a>
+                      <a href="#"><img src="./img/s1.png"  alt=""></a>
+                    </div>
+                </div>
+            </div>
+            
+          <section class="section2">
+            <div class="row text-center ">
+                <h2 class="text"> What our clients say.</h2>
+                <div class="col-sm-4  ">
+                 <img src="./img/man.png" >
+                </div>
+                <div class="col-sm-4  pt-5">
+                <video width="90%" height="90%" controls>
+                        <source src="./img/v1.mp4" type="video/mp4">
+                        <source src="./img/v1.ogg" type="video/ogg">
+                      Your browser does not support the video tag.
+                </video>
+               </div>
+               <div class="col-sm-4  pt-5">
+                <video width="90%" height="90%" controls>
+                        <source src="./img/v2.mp4" type="video/mp4">
+                        <source src="./img/v2.ogg" type="video/ogg">
+                      Your browser does not support the video tag.
+                </video>
+               </div>
+                </div>
+            </div>
+        </section>
+        
+        <div class="container-fluid">
+                <div class="row ">
+                    <div class="col-sm-6  pb-5 bg">
+                    <h1 class="h1-text2 pt-5 text-center">Our Patners</h1>
+                        <p class="pt-5  text-center">We are constantly searching and testing
+                             for the best tools and services on the market. 
+                            We partner with these organizations to provide the best
+                             service and value for our clients..</p>
+                      
+                    </div>
+                    <div class="col-sm-6  d-table pt-5 ">
+                    
+                      <a href="#"><img src="./img/pg2.png" class="pt-2"  alt=""></a>
+                      <a href="#"><img src="./img/pg3.png"class="pt-2"  alt=""></a>
+                      <a href="#"><img src="./img/pg6.png" class="pt-2" alt=""></a>  
+                      <a href="#"><img src="./img/pg4.png" class="pt-2" alt=""></a>
+                      <a href="#"><img src="./img/pg5.png" class="pt-2" alt=""></a>  
+                      <a href="#"><img src="./img/pg6.png" class="pt-5" alt=""></a>  
+                      <a href="#"><img src="./img/pg7.png" class="pt-5" alt=""></a>
+                      <a href="#"><img src="./img/pg8.png" class="pt-5" alt=""></a>
+                      <a href="#"><img src="./img/pg4.png" class="pt-5" alt=""></a>
+                      <a href="#"><img src="./img/pg6.png" class="pt-5" alt=""></a>  
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer class="text-center  ">
+          <div class="container-fluid">
+            <div class="row bg-black ">
+              <p class="p2 pt-5"> © 2022 by FIT Solutions. Managed IT Services, IT Consulting, Cybersecurity, and Cloud Hosting.</p>
+              <div class="d-inline-table ">
+                      <i class="fa fa-facebook aws" aria-hidden="true"></i>
+                      <i class="fa fa-youtube aws" aria-hidden="true"></i>
+                      <i class="fa fa-twitter aws" aria-hidden="true"></i>
+                      <i class="fa fa-instagram aws" aria-hidden="true"></i>
+                    </div> 
+            </div>
+         </div>
+       </footer>
+       <script src="./js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript">
+      var nav = document.querySelector('nav');
+
+      window.addEventListener('scroll', function () {
+        if (window.pageYOffset > 100) {
+          nav.classList.add('bg-dark', 'shadow');
+        } else {
+          nav.classList.remove('bg-dark', 'shadow');
+        }
+      });
+    </script>
+ </body>
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+  
+</html>
